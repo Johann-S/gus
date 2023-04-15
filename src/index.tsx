665429731806218b7a788bd71fe4589311311eb5
Johann-S <johann.servoire@gmail.com>
2023-04-15 14:07:16 +0200

    setup a linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 
 /** Styles */
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const root = createRoot(document.getElementById('root') as Element);
+const root = createRoot(document.getElementById('root') as Element)
 
 root.render(
   (
     <HelmetProvider>
       <h1>GUS - Github user search by Johann-S</h1>
     </HelmetProvider>
-  ),
-);
+  )
+)
